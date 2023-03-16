227facca4bb26d97c0e2a1bf1bef77a185ab1078
nesh191 <kamande.nesh@gmail.com>
2023-03-16 13:18:08 -0700

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - this is the main function
- *
- * Return - 0 when successful
- */
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
