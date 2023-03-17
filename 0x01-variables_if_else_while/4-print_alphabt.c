d4da65c97af3ec92a5ccd01a809370f462eb569d
nesh191 <kamande.nesh@gmail.com>
2023-03-17 13:45:55 -0700

    alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - writes alphabet in lowercase except q and e
+ * followed by a new line
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if(ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+		putchar('\n');
+
+	return (0);
+
+}
